32bf86da037c1d52188b406d5a5c021e2e03a90b
2AM-official <zhaoxinglyu@icloud.com>
2022-05-15 10:49:36 -0700

    log print

--- dormit/src/App.js
+++ dormit/src/App.js
@@ -1,13 +1,18 @@
 import Login from "./components/Login";
 
 function App() {
+  const addLogin = (task) => {
+    console.log(task)
+  }
+  
   return (
     <div className="container">
       <div className="left-half">
       </div>
       <div className="right-half">
         <h1>Welcome!</h1>
-        <Login />
+        <Login onAdd={addLogin}/>
+        <h5>Already have a Dormit Account?</h5>
       </div>
     </div>
   );
